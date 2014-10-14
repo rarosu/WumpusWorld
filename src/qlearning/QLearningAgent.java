54bbd604684fcc31fb25acb80140fc6f1a8873ab
Lars Woxberg <lars.woxberg@gmail.com>
2014-10-14 12:02:53 +0200

    First commit
    Setup my own agent class in the base project.

---
+++ src/qlearning/QLearningAgent.java
@@ -0,0 +1,23 @@
+package qlearning;
+
+import wumpusworld.*;
+
+/**
+ * This agent makes decisions
+ * @author Rarosu
+ */
+public class QLearningAgent {
+    private World world;
+    
+    public QLearningAgent(World world) {
+        this.world = world;
+        
+        // TODO: Load Q-Values
+    }
+    
+    public void doAction()
+    {
+        // TODO: Update knowledge base
+        // TODO: Make a decision
+    }
+}
